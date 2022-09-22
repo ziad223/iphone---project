0d102a9992e1afb847c0758ce411cbc952bf94b5
Ziad Abdalla <105611439+ziad223@users.noreply.github.com>
2022-09-23 01:12:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var img = document.querySelector(".img");
+var container = document.querySelector(".container");
+
+function phone(phone){
+    img.src = phone;
+}
+
+function colors(color){
+   container.style.background = color;
+}
+
+
